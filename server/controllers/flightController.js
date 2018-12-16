6597d2b4222b689f86cc93edf6a1911d4d1102c7
frozenfroggie <wojtyraj@gmail.com>
2018-12-16 20:40:38 +0100

    Move all amadeus api requests to test.api.amadeus.com/v1/

--- server/controllers/flightController.js
+++ server/controllers/flightController.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 const geoip = require('geoip-lite');
+const airports = require('../airports2.json');
+
+let authorizationHeader;
 
 const getNearestLocations = async (ll) => {
   try {
-    const response = await axios.get(`https://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant?apikey=${process.env.AMADEUS_API_KEY}&latitude=${ll[0]}&longitude=${ll[1]}`);
+    const response = await axios.get(`https://test.api.amadeus.com/v1/reference-data/locations/airports?latitude=${ll[0]}&longitude=${ll[1]}`, { headers: { Authorization: authorizationHeader } });
     const nearestLocations = response.data;
     return nearestLocations;
   } catch(err) {
-    throw new Error('Error in getting nearest location');
+    throw err;
   }
 }
 
@@ -23,22 +26,35 @@ const getDateRange = () => {
 
 const getInspirations = async (nearestAirport, dateRange) => {
   try {
-    const response = await axios.get(`https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=${process.env.AMADEUS_API_KEY}&origin=${nearestAirport}&departure_date=${dateRange.from}--${dateRange.to}&aggregation_mode=COUNTRY`);
-    const inspirations = response.data.results.splice(0,6);
-    const currency = response.data.currency;
+    const response = await axios.get(`https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=${nearestAirport}&departureDate=${dateRange.from},${dateRange.to}&viewBy=COUNTRY`, { headers: { Authorization: authorizationHeader } });
+    const inspirations = response.data.data;
+    const currency = response.data.meta.currency;
     return { inspirations, currency };
   } catch(err) {
-    throw new Error('Error in getting inspirations');
+    console.log(err.response.data);
+    return null;
+    // throw new Error('Error in getting inspirations');
   }
 }
 
 const getLocationInfo = async (destination) => {
   try {
-    const response = await axios.get(`https://api.sandbox.amadeus.com/v1.2/location/${destination}?apikey=${process.env.AMADEUS_API_KEY}`);
-    const locationInfo = response.data;
-    return locationInfo;
+    const airport = airports.find(airport => airport.iata === destination);
+    let response;
+    if(airport && airport.lat && airport.lon) {
+      response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${airport.lat},${airport.lon}&key=${process.env.GOOGLE_API_KEY}`);
+    }
+    if(response && response.data) {
+      return {
+        city: response.data,
+        airport
+      }
+    } else {
+      return null
+    }
   } catch(err) {
-    throw new Error('Error in getting location info');
+    debugger;
+    throw err;
   }
 }
 
@@ -49,55 +65,95 @@ module.exports = {
       // find client latitude and longitude coordinates based on ip
       let ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
       // For testing purposes, set ip to 207.97.227.239 to work on localhost
-      ip = "212.83.162.129";
+      ip = "185.126.66.172";
       const { ll } = geoip.lookup(ip);
-
+      authorizationHeader = req.headers['authorization'];
+      const accessToken = authorizationHeader.split(" ")[1];
+      console.log(authorizationHeader);
+      console.log(accessToken);
+      if(!accessToken || accessToken === 'undefined') {
+        console.log('Not authenticated')
+        debugger;
+        return res.status(401).json({
+          msg: 'Not authenticated'
+        });
+      }
       // get most relevant airports in a radius of 500 km around the client coordinates
       const nearestLocations = await getNearestLocations(ll);
-      let nearestLocation = nearestLocations.reduce((previousLocation, currentLocation) => {
-        return (currentLocation.distance < previousLocation.distance) ? currentLocation : previousLocation;
-      });
+      // console.log(nearestLocations)
+      // let nearestLocation = nearestLocations.data.reduce((previousLocation, currentLocation) => {
+      //   return (currentLocation.distance < previousLocation.distance) ? currentLocation : previousLocation;
+      // });
+      // console.log(nearestLocation);
       // For testing purposes, set nearest airport to PAR (Paris), sandbox API doesnt return any results for aiports in my country (Poland)
-      nearestLocation.airport = 'PAR';
+      // nearestLocation.airport = 'PAR';
 
       // get inspirations
       const dateRange = getDateRange();
-      const { inspirations, currency } = await getInspirations(nearestLocation.airport, dateRange);
+      let inspirations;
+      let currency;
+      for(let i = 0; i < nearestLocations.data.length; i++) {
+        const response = await getInspirations(nearestLocations.data[i].iataCode, dateRange);
+        if(response && response.inspirations) {
+          inspirations = response.inspirations;
+          currency = response.currency;
+          break;
+        }
+      }
       let inspirationsNum = 0;
-      for(let i = 0; i < 6; i++) {
+      if(!inspirations) {
+        res.json({results: []});
+      }
+      for(let i = 0; inspirationsNum < 6; i++) {
+        console.log(inspirationsNum)
         const inspiration = inspirations[i];
         const destination = inspiration.destination;
         const price = inspiration.price;
         // get additional info about destination
         const locationInfo = await getLocationInfo(destination);
-        inspiration.location_info = locationInfo;
-        inspiration.origin = nearestLocation.airport;
+        console.log("locationInfo", locationInfo)
+        if(locationInfo) {
+          inspirationsNum++;
+          inspiration.location_info = locationInfo;
+        }
+        inspiration.origin = nearestLocations.data[0].iataCode;
         inspiration.originLocation = ll;
       }
 
       res.json(inspirations);
     } catch(err) {
       console.log(err);
+      debugger;
+      if(err.response.status === 401) {
+        return res.status(401).json({
+          msg: 'Not authenticated'
+        });
+      }
       res.status(500).json(err);
     }
   },
   autocomplete: async (req, res) => {
     try {
       const term = req.params.term;
-      const response = await axios.get(`https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=${process.env.AMADEUS_API_KEY}&term=${term}`)
-      res.json(response.data);
+      const response = await axios.get(`https://test.api.amadeus.com/v1/reference-data/locations?view=LIGHT&subType=AIRPORT&keyword=${term}`, { headers: { Authorization: authorizationHeader } })
+      res.json(response.data.data);
     } catch(err) {
-      console.log(err);
+      debugger;
+      if(err.response.status === 401) {
+        return res.status(401).json({
+          msg: 'Not authenticated'
+        });
+      }
       res.status(500).json({msg: err});
     }
   },
   lowFareSearch: async (req, res) => {
     try {
       const { origin, destination, departure_date, return_date, travel_class, adults, currency } = req.query;
-      const response = await axios.get(`https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=${process.env.AMADEUS_API_KEY}&origin=${origin}&destination=${destination}&departure_date=${departure_date}&number_of_results=5${return_date !== '' ? '&return_date=' + return_date : ''}&adults=${adults}&travel_class=${travel_class}&currency=${currency}`)
-      res.json(response.data);
+      const response = await axios.get(`https://test.api.amadeus.com/v1/shopping/flight-offers?origin=${origin}&destination=${destination}&departureDate=${departure_date}&max=5${return_date !== '' ? '&returnDate=' + return_date : ''}&adults=${adults}&travelClass=${travel_class}&currency=${currency}`, { headers: { Authorization: authorizationHeader } })
+      res.json(response.data.data);
     } catch(err) {
-      if(err.response.data.message === 'No result found.') {
+      if(err.response.data.errors[0].status === 404) {
         res.status(404).json({msg: err.response.data.message});
       }
       res.status(500).json({msg: err});
